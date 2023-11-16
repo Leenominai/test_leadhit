7552043c638ec7b3fff02378e77c3098c779464d
Александр Рассыхаев <farexzon@yandex.ru>
2023-11-16 17:57:04 +0300

    Develop v1.61

--- backend/test_requests.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-# Тестовый запрос 1
-curl -X POST -d "f_name1=value1&f_name2=value2" http://test_backend:5000/get_form
-
-# Тестовый запрос 2
-curl -X POST -d "f_name1=invalid_phone" http://test_backend:5000/get_form
