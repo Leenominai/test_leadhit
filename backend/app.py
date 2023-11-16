fdfa8ae59a7c177cdf060dfdde625b45227f6b98
Александр Рассыхаев <farexzon@yandex.ru>
2023-11-16 13:59:39 +0300

    Develop v1.2

--- backend/app.py
+++ backend/app.py
@@ -1,90 +1,72 @@
-import os
 import re
-import sys
-
+import logging
 from flask import Flask, request, jsonify
-from pymongo import MongoClient
+
 app = Flask(__name__)
 
-# Подключение к MongoDB
-try:
-    client = MongoClient(os.getenv("MONGO_URL"))
-    db = client.forms
-    templates_collection = db.templates
-except Exception as e:
-    print(f"Error connecting to MongoDB: {e}")
-    sys.exit(1)
+# Настройка логгера
+logging.basicConfig(level=logging.DEBUG)
+logger = logging.getLogger(__name__)
 
 
+# Define validation functions for phone, date, and email
 def validate_phone(phone):
-    # Валидация телефона: формат +7 xxx xxx xx xx
     phone_pattern = re.compile(r'^\+7 \d{3} \d{3} \d{2} \d{2}$')
     return bool(re.match(phone_pattern, phone))
 
 
 def validate_date(date):
-    # Валидация даты: формат DD.MM.YYYY или YYYY-MM-DD
     date_pattern1 = re.compile(r'^\d{2}.\d{2}.\d{4}$')
     date_pattern2 = re.compile(r'^\d{4}-\d{2}-\d{2}$')
     return bool(re.match(date_pattern1, date) or re.match(date_pattern2, date))
 
 
 def validate_email(email):
-    # Валидация email
     email_pattern = re.compile(r'^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$')
     return bool(re.match(email_pattern, email))
 
 
-def process_form_data(data):
-    processed_data = {}
-    errors = {}
+def validate(field_type, value):
+    if field_type == "date":
+        return validate_date(value)
+    elif field_type == "phone":
+        return validate_phone(value)
+    elif field_type == "email":
+        return validate_email(value)
+    # Добавьте другие типы по мере необходимости
+    elif field_type == "text":
+        # Валидация текста (пример: всегда валиден)
+        return True
+    else:
+        # Неизвестный тип, считаем невалидным
+        return False
 
-    for key, value in data.items():
-        if key == "phone" and not validate_phone(value):
-            errors[key] = "Invalid phone format"
-        elif key == "date" and not validate_date(value):
-            errors[key] = "Invalid date format"
-        elif key == "email" and not validate_email(value):
-            errors[key] = "Invalid email format"
-        else:
-            processed_data[key] = value
 
-    return processed_data, errors
+def typeify_fields(input_data):
+    types_priority = ["date", "phone", "email", "text"]
+    types = {}
 
+    for field, value in input_data.items():
+        for field_type in types_priority:
+            if validate(field_type, value):
+                types[field] = field_type
+                break
 
-def get_form_name(form_data):
-    for template in templates_collection.find():
-        template_fields = set(template.keys()) - {"_id", "name"}
-        form_fields = set(form_data.keys())
-        if template_fields.issubset(form_fields):
-            matching_fields = [field for field in template_fields if template[field] == form_data[field]]
-            if set(matching_fields) == template_fields:
-                return template["name"]
-
-    # Если подходящей формы не найдено, типизируем поля на лету
-    field_types = {
-        field: "date" if validate_date(form_data[field]) else
-               "phone" if validate_phone(form_data[field]) else
-               "email" if validate_email(form_data[field]) else
-               "text"
-        for field in form_data
-    }
-
-    return field_types
+    return types
 
 
 @app.route('/get_form', methods=['POST'])
 def get_form():
-    try:
-        form_data, errors = process_form_data(request.form.to_dict())
-        if errors:
-            return jsonify({"error": "Invalid form fields", "errors": errors}), 400
+    # Логирование полученных данных
+    input_data = request.form.to_dict()
+    logger.info(f"Received input data: {input_data}")
 
-        result = get_form_name(form_data)
-        return jsonify({"result": result})
-    except Exception as e:
-        return jsonify({"error": str(e)}), 500
+    # Логирование типизации полей
+    field_types = typeify_fields(input_data)
+    logger.info(f"Field types: {field_types}")
+
+    return jsonify(field_types)
 
 
-if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000, debug=True)
+if __name__ == '__main__':
+    app.run(debug=True)
--- backend/test_requests.py
+++ backend/test_requests.py
@@ -1,28 +1,60 @@
 import requests
+import json
+import logging
 
-# URL вашего приложения
+
+logging.basicConfig(level=logging.INFO)
+
 base_url = "http://localhost:5000"
 
 
-# Пример тестового запроса с данными формы
-def test_form_request():
+def test_form_request(form_data):
     url = f"{base_url}/get_form"
-
-    # Данные формы в виде словаря
-    form_data = {
-        "f_name1": "john.doe@example.com",
-        "f_name2": "+7 123 456 78 90"
-        # Добавьте другие поля формы по мере необходимости
-    }
-
-    # Отправка POST-запроса с данными формы
     response = requests.post(url, data=form_data)
 
-    # Вывод результата запроса
-    print(f"Response Status Code: {response.status_code}")
-    print("Response Content:")
-    print(response.text)
+    # Получаем ожидаемый ответ из тестовой базы
+    with open("test_templates.json", "r") as file:
+        templates = json.load(file)
+
+    # Сравниваем ответ веб-приложения с ожидаемым ответом
+    for template in templates:
+        if set(form_data.keys()) == set(template.keys()):
+            return template["name"]
+
+    # # Если не найдено совпадений, типизируем поля на лету
+    # return {field: type(form_data[field]) for field in form_data}
+
+    # Если не найдено совпадений, типизируем поля на лету
+    field_types = {field: type(form_data[field]) for field in form_data}
+
+    # Проверяем, соответствуют ли типы полей требованиям какой-либо формы
+    for template in templates:
+        if all(
+            isinstance(field_value, template.get(field, {}).get("type"))
+            for field, field_value in field_types.items()
+        ):
+            return template["name"]
+
+    # Если ни одна форма не соответствует требованиям
+    return field_types
 
 
-# Вызываем функцию для выполнения тестового запроса
-test_form_request()
+# Примеры тестовых данных
+test_data = [
+    # Валидные данные, совпадение с известной формой
+    {"email": "john.doe@example.com", "phone": "+7 123 456 78 90"},
+    {"date": "2022-01-15", "text": "Some text"},
+    {"phone": "+7 123 456 78 90", "email": "jane.smith@example.com"},
+    {"email": "invalid_email", "phone": "+7 123 45"},
+    {"date": "15.01.2022", "text": "Some text"},
+    {"phone": "+1 (555) 123-4567", "email": "jane.smith@company.co"},
+    {"custom_field": "Some text with !@#$%^&*()_+"},
+    {"text_field": "A longer piece of text with multiple words"},
+    {"html_field": "<p>This is a paragraph.</p><br><p>Another paragraph.</p>"}
+]
+
+# Загрузка тестовой базы шаблонов форм из файла
+for data in test_data:
+    result = test_form_request(data)
+    print(f"Входные данные: {data}")
+    print(f"Выходные данные: {result}")
